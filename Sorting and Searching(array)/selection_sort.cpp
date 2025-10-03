db05c5aec106f88df0c6c5b99f095413561923ee
Sanskar Tiwari <sanskartiwari.smt@gmail.com>
2025-10-03 14:51:06 +0530

    Update selection_sort.cpp

--- Sorting and Searching(array)/selection_sort.cpp
+++ Sorting and Searching(array)/selection_sort.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 void selection(int arr[], int n) {
-    for (int a = 0; a < n; a++) {
-        for (int b = a; b < n; b++) {
-            if (arr[a] > arr[b]) {
-                swap(arr[a], arr[b]);
+    for (int a = 0; a < n - 1; a++) {
+        int minIndex = a;  // assume current is min
+        for (int b = a + 1; b < n; b++) {
+            if (arr[b] < arr[minIndex]) {
+                minIndex = b;  // found smaller element
             }
         }
+        // Swap only once after finding minimum
+        swap(arr[a], arr[minIndex]);
     }
     display(arr, n);
 }
